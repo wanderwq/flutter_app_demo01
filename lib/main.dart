9714b6fc785d76fa56edca8ae7dd287827773535
12 <121>
2020-08-16 21:33:51 +0800

    初次提交

---
+++ lib/main.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import './movie/list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHome(
+      ),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length:3,
+        child:
+        Scaffold(
+          appBar: AppBar(        title: Text("电影列表"),
+            centerTitle:true,
+            actions: <Widget>[
+              IconButton(
+                icon: new Icon(Icons.search),
+                onPressed: (){},
+              )
+            ],
+          ),
+          drawer: new Drawer(
+            child: new ListView(
+              padding: EdgeInsets.all(0),
+              children: <Widget>[
+                new UserAccountsDrawerHeader(
+                  accountName:Text("wander"),
+                  accountEmail:Text("111111"),
+                  currentAccountPicture:CircleAvatar(
+                      backgroundImage:new NetworkImage(
+                          "http://img8.zol.com.cn/bbs/upload/23765/23764201.jpg"
+                      )
+                  ),
+                  decoration: BoxDecoration(
+
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            "http://img8.zol.com.cn/bbs/upload/23765/23764201.jpg")),
+                  ),
+
+                ),
+                ListTile(title:Text("用户反馈"),trailing: Icon(Icons.feedback),),
+                ListTile(title:Text("系统设置"),trailing: Icon(Icons.settings),),
+                ListTile(title:Text("我要发布"),trailing: Icon(Icons.send),),
+                Divider(color:Colors.blue),
+                ListTile(title:Text("注销"),trailing: Icon(Icons.exit_to_app),),
+              ],),
+          ),
+          //底部
+          bottomNavigationBar: Container(
+            decoration:BoxDecoration(color: Colors.blue),
+            height: 50,
+            child: TabBar(
+              labelStyle: TextStyle(
+                height: 0,
+                fontSize: 12
+              ),
+              tabs: <Widget>[
+              Tab(icon: Icon(Icons.movie_filter),text: "正在上映",),
+              Tab(icon: Icon(Icons.movie_creation),text: "即将上映",),
+              Tab(icon: Icon(Icons.local_movies),text: "Top25",),
+            ],),
+          ),
+          body: TabBarView(children: [
+            MovieList(mt: "in_theaters",),
+            MovieList(mt: "coming_soon",),
+            MovieList(mt: "top250",),
+          ],),
+        )
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/movie/list.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+Dio dio = new Dio();
+
+class MovieList extends StatefulWidget {
+  //固定用法
+  MovieList({Key key, @required this.mt}) :super(key: key);
+  final String mt;
+
+  @override
+  _MovieListState createState() {
+    return new _MovieListState();
+  }
+}
+
+//有状态控件，必须结合一个状态管理类 来进行实现
+class _MovieListState extends State<MovieList> {
+  int page = 1;
+  int pageSize = 10;
+  var mlist = [];
+  var total=0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getMoviList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build 
+    return ListView.builder(
+        itemCount: mlist.length,
+        itemBuilder: (BuildContext ctx,int i){
+          //每次循环
+         var mitem= mlist[i];
+//         return Text(mitem['title']);R
+          //列
+         return Container(
+           height: 200,
+           decoration: BoxDecoration(color: Colors.white,border: Border(top: BorderSide(color: Colors.black12))),
+           child: Row(children: [
+             Image.network(mitem['images']['small'],width: 130,height: 180,fit: BoxFit.cover,),
+             Container(
+               padding: EdgeInsets.only(left: 10),
+               height: 200,
+               child: Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+                 children: [
+                   Text("电影名称:${mitem["title"]}"),
+                   Text("上映年份:${mitem["year"]}年"),
+                   Text("电影类型:${mitem["genres"].join(',')}"),
+                   Text("豆瓣评分:${mitem["rating"]['average']}分"),
+                   Text("主要演员:${mitem["title"]}")
+                 ],),
+             )
+           ],),
+         );
+
+        });
+//    return Text("列表----+" + widget.mt+"------${mlist.length}");
+  }
+
+  getMoviList() async{
+    int offset=(page-1)*pageSize;
+    //偏移量
+   var response=await dio.get('http://www.liulongbin.top:3005/api/v2/movie/${widget.mt}?start=$offset#count=$pageSize');
+   var result=response.data;
+   print(result);
+
+   //今后只要为私有数据赋值，都要把数据
+   setState(() {
+    mlist=result["subjects"];
+    total=result["total"];
+   });
+  }
+}
